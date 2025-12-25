161631b93d0727ec4c5f968fd8ac66ac4732feac
esmail mohamed <em5494282@gmail.com>
2025-12-25 21:34:09 +0200

    Keep firebase_options.dart local, hide keys

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,15 @@
+import 'package:book_app/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'core/routes/app_pages.dart';
 import 'core/routes/app_routes.dart';
 
-Future<void> main() async {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-
-  await dotenv.load(fileName: ".env");
-
   await Firebase.initializeApp(
-    options: FirebaseOptions(
-      apiKey: dotenv.env['FIREBASE_API_KEY']!,
-      appId: dotenv.env['FIREBASE_APP_ID']!,
-      messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
-      projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
-      storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
-      authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN'],
-      measurementId: dotenv.env['FIREBASE_MEASUREMENT_ID'],
-    ),
+    options: DefaultFirebaseOptions.currentPlatform,
   );
-
   runApp(const MyApp());
 }
 
