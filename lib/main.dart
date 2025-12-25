5e15a3596dbd92b13827990aaa492330f9472f9c
esmail mohamed <em5494282@gmail.com>
2025-12-25 21:18:46 +0200

    "Save current changes before rewriting history"

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,25 @@
-import 'package:book_app/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart'; // <--- استيراد dotenv
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'core/routes/app_pages.dart';
 import 'core/routes/app_routes.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await dotenv.load(fileName: ".env");
 
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+    options: FirebaseOptions(
+      apiKey: dotenv.env['FIREBASE_API_KEY']!,
+      appId: dotenv.env['FIREBASE_APP_ID']!,
+      messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
+      projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
+      storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
+      authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN'],
+      measurementId: dotenv.env['FIREBASE_MEASUREMENT_ID'],
+    ),
   );
 
   runApp(const MyApp());
